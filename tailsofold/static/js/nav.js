e24170c24851d1e0026b8dc557556e131b0730bf
isaac.french <isaac.french@exa.net.uk>
2026-06-25 00:46:10 +0100

    Updates!

--- tailsofold/static/js/nav.js
+++ tailsofold/static/js/nav.js
@@ -11,6 +11,23 @@ function toggleTheme() {
     localStorage.theme = "dark";
 }
 
+function toggleThemeMenu() {
+    var menu = $("#themeMenu")
+    if (menu.hasClass("hidden")) {
+        menu.removeClass("hidden")
+        return
+    }
+    menu.addClass("hidden")
+}
+
+$("#hueRange").on("input", function () {
+    var hue = $("#hueRange").val();
+    console.log(hue);
+    document.documentElement.style.setProperty("--hue", hue)
+    document.documentElement.style.setProperty("hue", hue)
+
+})
+
 if (localStorage.theme) {
     $("body").addClass(localStorage.theme);
 } else {
